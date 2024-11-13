aafb5c7d46df7a83a8502a5e437040468f06b5dc
Koeun Jeong <swanyjke@gmail.com>
2024-11-13 16:08:52 +0900

    [fix] home font size

--- src/page/FindLiking.jsx
+++ src/page/FindLiking.jsx
@@ -8,33 +8,7 @@ const FindLiking = () => {
   const [name, setName] = useState("");
   const [imageUrl, setImageUrl] = useState("");
 
-  const fetchImage = async (keyword) => {
-    //   try {
-    //     const response = await fetch(
-    //       `https://api.pinterest.com/v5/search/pins?query=${encodeURIComponent(
-    //         keyword
-    //       )}`,
-    //       {
-    //         headers: {
-    //           Authorization: `Bearer ${accessToken}`,
-    //           "Content-Type": "application/json",
-    //         },
-    //       }
-    //     );
-    //     if (!response.ok) {
-    //       throw new Error(`HTTP error! status: ${response.status}`);
-    //     }
-    //     const data = await response.json();
-    //     if (data.items && data.items.length > 0) {
-    //       setImageUrl(data.items[0].media.images.original.url);
-    //     } else {
-    //       alert("이미지를 찾을 수 없습니다.");
-    //     }
-    //   } catch (error) {
-    //     console.error("Error fetching image:", error);
-    //     alert("이미지를 불러오는 중 오류가 발생했습니다.");
-    //   }
-  };
+  const fetchImage = async (keyword) => {};
 
   const handleKeywordChange = (index, value) => {
     const newKeywords = [...keywords];
@@ -109,7 +83,6 @@ const AppContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  font-family: Pretendard;
   text-align: center;
   margin-top: 50px;
 `;
@@ -131,6 +104,7 @@ const Description = styled.div`
   color: #333;
   margin-bottom: 78px;
   line-height: 40px;
+  font-family: Pretendard;
 `;
 
 const KorDescription = styled.div`
@@ -167,6 +141,7 @@ const KeywordBox = styled.input`
 `;
 
 const NameInputSection = styled.div`
+  font-family: Pretendard;
   margin-top: 40px;
   text-align: center;
   display: flex;
